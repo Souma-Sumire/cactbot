7f15293794730ee20e34ba6fc507f529b4c33280
Souma-Sumire <553469159@qq.com>
2026-06-27 14:22:02 +0800

    打铁

--- ui/raidboss/data/99-custom/souma/绝 - 妖星.ts
+++ ui/raidboss/data/99-custom/souma/绝 - 妖星.ts
@@ -176,6 +176,9 @@ export interface Data extends RaidbossData {
     p2一运搭档打法: 'same' | 'free';
     p2一运是扇形组左钢铁组右吗: 'yes' | 'no';
     // p2一运1234打法没debuff的闲人怎么决定去哪个塔引导: 'TN左DPS右';
+    p3打铁警察: boolean;
+    p3打铁聊天频道?: 'e' | 'p';
+    p3打铁监控范围: 'me' | 'all';
   };
   // General
   phase: Phase | 'unknown';
@@ -450,6 +453,43 @@ const triggerSet: TriggerSet<Data> = {
         },
       },
       default: 'no',
+    },
+    {
+      id: 'p3打铁警察',
+      name: {
+        en: '开启P3打铁警察',
+      },
+      type: 'checkbox',
+      default: true,
+      comment: { en: '需要插件“鲶鱼精邮差”' },
+    },
+    {
+      id: 'p3打铁聊天频道',
+      name: {
+        en: '打铁发送到聊天频道',
+      },
+      type: 'select',
+      options: {
+        en: {
+          '默语': 'e',
+          '小队（破坏队伍团结，极其不推荐）': 'p',
+        },
+      },
+      default: 'e',
+    },
+    {
+      id: 'p3打铁监控范围',
+      name: {
+        en: '打铁监控范围',
+      },
+      type: 'select',
+      options: {
+        en: {
+          '仅自己': 'me',
+          '所有人': 'all',
+        },
+      },
+      default: 'all',
     },
   ],
   overrideTimelineFile: true,
@@ -1444,6 +1484,57 @@ hideall "准备魔击x3"
       type: 'StartsUsing',
       netRegex: { id: 'C3F7', capture: false },
       response: Responses.getUnder('alert'),
+    },
+    {
+      id: 'DMU P3 简易打铁警察',
+      type: 'Ability',
+      netRegex: { targetId: '4.{7}' },
+      condition: (data, matches) => {
+        if (data.triggerSetConfig.p3打铁警察 === false)
+          return false;
+        if (data.phase !== 'p3')
+          return false;
+        if (!matches.targetId.startsWith('4'))
+          return false;
+        // AOE技能，因为判断太复杂
+        if (matches.type === '22') {
+          return false;
+        }
+        if ((data.triggerSetConfig.p3打铁监控范围 ?? 'all') === 'me') {
+          if (matches.source !== data.me)
+            return false;
+        } else {
+          if (!data.party.partyNames.includes(matches.source))
+            return false;
+        }
+        // 自动攻击
+        if (['07', '08'].includes(matches.id))
+          return false;
+        if (matches.targetIndex !== '0')
+          return false;
+        const damageVal = parseInt(matches.damage ?? '0', 16);
+        if (damageVal !== 0) {
+          return false;
+        }
+        const flagVal = parseInt(matches.flags ?? '0', 16);
+        return (flagVal & 0xFF) === 7;
+      },
+      infoText: (data, matches, output) => {
+        const channel = data.triggerSetConfig.p3打铁聊天频道 ?? 'e';
+        const name = data.party.member(matches.source);
+        const ability = matches.ability;
+        const text = output.singleTarget!({ name, ability });
+        void callOverlayHandler({
+          call: 'PostNamazu',
+          c: 'DoTextCommand',
+          p: `/${channel} ${text}`,
+        });
+        return undefined;
+        // return text;
+      },
+      outputStrings: {
+        singleTarget: '恭喜 ${name.job} 打铁成功！${ability}<se.5>',
+      },
     },
     {
       id: 'DMU P3 debuff',
