398e679ff8f95aab7f9af64723d6bb117c4d1309
Souma-Sumire <553469159@qq.com>
2026-07-08 08:55:29 +0800

    Update 绝 - 妖星.ts

--- ui/raidboss/data/99-custom/souma/绝 - 妖星.ts
+++ ui/raidboss/data/99-custom/souma/绝 - 妖星.ts
@@ -460,7 +460,7 @@ const triggerSet: TriggerSet<Data> = {
       id: 'p3混沌之土标记',
       name: { en: '开启P3混沌之土标记' },
       type: 'checkbox',
-      default: true,
+      default: false,
       comment: { en: '需要插件“鲶鱼精邮差”' },
     },
     {
